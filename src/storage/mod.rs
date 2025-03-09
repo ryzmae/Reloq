d591f8e87089f9d007df1c982b007935ceb52d3a
ryzm <114670723+ryzmae@users.noreply.github.com>
2025-03-09 15:04:45 +0100

    fix: SQL statements

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -15,18 +15,18 @@ impl Storage {
     pub fn init_tables(&self) -> Result<()> {
         self.conn.execute_batch(
             "
-                CREATE IF NOT EXISTS TABLE rate_limits (
-                    user_id TEXT PRIMARY KEY,
-                    limit INTEGER,
-                    last_updated INTEGER
-                );
+        CREATE TABLE IF NOT EXISTS rate_limits (
+            user_id TEXT PRIMARY KEY,
+            request_limit INTEGER,
+            last_updated INTEGER
+        );
 
-                CREATE TABLE IF NOT EXISTS job_queue (
-                    job_id TEXT PRIMARY KEY,
-                    payload TEXT NOT NULL,
-                    created_at INTEGER NOT NULL,
-                );
-                ",
+        CREATE TABLE IF NOT EXISTS job_queue (
+            job_id TEXT PRIMARY KEY,
+            payload TEXT NOT NULL,
+            created_at INTEGER NOT NULL
+        );
+        ",
         )?;
         Ok(())
     }
@@ -34,12 +34,12 @@ impl Storage {
     /// Inserts or updates a rate limit for a user.
     pub fn set_rate_limit(&self, user_id: &str, limit: u64) -> Result<()> {
         self.conn.execute(
-            "INSERT INTO rate_limits (user_id, limit, last_updated)
-                 VALUES (?1, ?2,  strftime('%s', 'now'))
-                ON CONFLICT(user_id) DO UPDATE SET limit = excluded.limit, last_updated = excluded.last_updated;",
+            "INSERT INTO rate_limits (user_id, request_limit, last_updated)
+         VALUES (?1, ?2, strftime('%s', 'now'))
+         ON CONFLICT(user_id) DO UPDATE 
+         SET request_limit = excluded.request_limit, last_updated = excluded.last_updated;",
             (user_id, limit),
         )?;
-
         Ok(())
     }
 
@@ -47,7 +47,7 @@ impl Storage {
     pub fn get_rate_limit(&self, user_id: &str) -> Result<Option<u64>> {
         let mut stmt = self
             .conn
-            .prepare("SELECT limit FROM rate_limits WHERE user_id = ?1")?;
+            .prepare("SELECT request_limit FROM rate_limits WHERE user_id = ?1")?;
         let mut rows = stmt.query([user_id])?;
 
         if let Some(row) = rows.next()? {
